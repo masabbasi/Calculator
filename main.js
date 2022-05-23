cebe33a2a5b9dbcbf8c03e5fe82a2f067220acff
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-05-23 20:53:30 +0430

    UI And Light/Dark Mode

---
+++ main.js
@@ -0,0 +1,29 @@
+const toggle = document.getElementById('toggle')
+let light = true;
+
+toggle.addEventListener('click',function(){
+    if (!light) {
+        localStorage.setItem('theme','light')
+        document.documentElement.style.setProperty("--main-color1","#ffffff")
+        document.documentElement.style.setProperty("--main-color2","#fafafa")
+        document.documentElement.style.setProperty("--number-color","#000000")
+        light = true;
+    } else {
+        localStorage.setItem('theme','dark')
+        light = false;
+        document.documentElement.style.setProperty("--main-color1","#191c21")
+        document.documentElement.style.setProperty("--main-color2","#252932")
+        document.documentElement.style.setProperty("--number-color","#fffffd")
+    }
+})
+
+window.onload = function () {
+    let localStorageTheme = localStorage.getItem('theme')
+    if (localStorageTheme ==='dark') {
+        document.documentElement.style.setProperty("--main-color1","#191c21")
+        document.documentElement.style.setProperty("--main-color2","#252932")
+        document.documentElement.style.setProperty("--number-color","#fffffd")
+        toggle.checked = true;
+        light = false;
+    }
+}
